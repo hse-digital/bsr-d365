f30deaf1c0418c69ff33dfbe66f1c5047b4a73bf
Philip Ryan <pryan@codec.ie>
2023-09-21 10:55:33 +0100

    JavaScript to hid tabs on BPA form based on Application Type #14025

--- Code/BSR.D365.WS3/BSR.D365.WS3.WebResources/js/bsr_buildingprofessionapplication/bsr_buildingprofessionapplication.js
+++ Code/BSR.D365.WS3/BSR.D365.WS3.WebResources/js/bsr_buildingprofessionapplication/bsr_buildingprofessionapplication.js
@@ -4,6 +4,55 @@ var BuildingProfessionApplicationForm;
     var Form;
     function Test(executionContext) {
         Form = executionContext.getFormContext();
+        var buildingProfessionType = Form.getAttribute("bsr_buildingprofessiontypecode").getValue();
+        if (buildingProfessionType === 760810000) {
+            Form.ui.tabs.get("Class").setVisible(true);
+            Form.ui.tabs.get("Competency").setVisible(true);
+            Form.ui.tabs.get("Memberships").setVisible(true);
+            Form.ui.tabs.get("Employment").setVisible(true);
+            Form.ui.tabs.get("rbcainfo").setVisible(false);
+            Form.getControl("bsr_applicantid").setVisible(true);
+            Form.getControl("bsr_applicantid1").setVisible(false);
+            Form.getControl("bsr_rbcasecondarycontactid").setVisible(false);
+            Form.getControl("bsr_secondarycontactposition").setVisible(false);
+            Form.ui.quickForms.get("QuickviewControl1694786591140").setVisible(false);
+            Form.ui.tabs.get("General").sections.get("additional_info").setVisible(false);
+            Form.getControl("bsr_primarycontactposition").setVisible(false);
+            Form.ui.quickForms.get("rbiprimarycontactquickview").setVisible(true);
+            Form.ui.quickForms.get("bcaprimarycontactquickview").setVisible(false);
+        }
+        else if (buildingProfessionType === 760810001) {
+            Form.ui.tabs.get("Class").setVisible(false);
+            Form.ui.tabs.get("Competency").setVisible(false);
+            Form.ui.tabs.get("Memberships").setVisible(false);
+            Form.ui.tabs.get("Employment").setVisible(false);
+            Form.ui.tabs.get("rbcainfo").setVisible(true);
+            Form.getControl("bsr_applicantid").setVisible(false);
+            Form.getControl("bsr_applicantid1").setVisible(true);
+            Form.getControl("bsr_rbcasecondarycontactid").setVisible(true);
+            Form.getControl("bsr_secondarycontactposition").setVisible(true);
+            Form.ui.quickForms.get("QuickviewControl1694786591140").setVisible(true);
+            Form.ui.tabs.get("General").sections.get("additional_info").setVisible(true);
+            Form.getControl("bsr_primarycontactposition").setVisible(true);
+            Form.ui.quickForms.get("bcaprimarycontactquickview").setVisible(true);
+            Form.ui.quickForms.get("rbiprimarycontactquickview").setVisible(false);
+        }
+        else {
+            Form.ui.tabs.get("Class").setVisible(false);
+            Form.ui.tabs.get("Competency").setVisible(false);
+            Form.ui.tabs.get("Memberships").setVisible(false);
+            Form.ui.tabs.get("Employment").setVisible(false);
+            Form.ui.tabs.get("rbcainfo").setVisible(false);
+            Form.getControl("bsr_applicantid").setVisible(false);
+            Form.getControl("bsr_applicantid1").setVisible(false);
+            Form.getControl("bsr_rbcasecondarycontactid").setVisible(false);
+            Form.getControl("bsr_secondarycontactposition").setVisible(false);
+            Form.ui.quickForms.get("QuickviewControl1694786591140").setVisible(false);
+            Form.ui.tabs.get("General").sections.get("additional_info").setVisible(false);
+            Form.getControl("bsr_primarycontactposition").setVisible(false);
+            Form.ui.quickForms.get("bcaprimarycontactquickview").setVisible(false);
+            Form.ui.quickForms.get("rbiprimarycontactquickview").setVisible(false);
+        }
     }
     BuildingProfessionApplicationForm.Test = Test;
 })(BuildingProfessionApplicationForm || (BuildingProfessionApplicationForm = {}));
