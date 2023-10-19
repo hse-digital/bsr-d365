09c9fad7606414a2a19e49c880723a53dff1b8c7
Malachy Keaveny <mkeaveny@codec.ie>
2023-10-19 15:21:48 +0100

    Hiding and showing sections around Staged Applications/Subsequent Staged Applications

--- Code/BSR.D365.WS2/BSR.D365.WS2.WebResources/js/bsr_buildingcontrolapplication/bsr_buildingcontrolapplication.js
+++ Code/BSR.D365.WS2/BSR.D365.WS2.WebResources/js/bsr_buildingcontrolapplication/bsr_buildingcontrolapplication.js
@@ -3,17 +3,18 @@ var BuildingControlApplication;
 (function (BuildingControlApplication) {
     var Form;
     function ShowHideStagedApplicationsTab(executionContext) {
-        Form = executionContext.getFormContext();
-        if (Form.getAttribute("bsr_stagedapplication").getValue() == false && Form.getAttribute("bsr_issubsequentstagedapp").getValue() == false) {
-            Form.ui.tabs.get("tab_staged_application").setVisible(false);
+        Form = (executionContext.getFormContext());
+        if (Form.getAttribute('bsr_stagedapplication').getValue() == false &&
+            Form.getAttribute('bsr_issubsequentstagedapp').getValue() == false) {
+            Form.ui.tabs.get('tab_staged_application').setVisible(false);
         }
         else {
-            Form.ui.tabs.get("tab_staged_application").setVisible(true);
-            if (Form.getAttribute("bsr_stagedapplication").getValue() == true) {
-                Form.getControl("Subgrid_new_2").setVisible(true);
+            Form.ui.tabs.get('tab_staged_application').setVisible(true);
+            if (Form.getAttribute('bsr_stagedapplication').getValue() == true) {
+                Form.getControl('Subgrid_new_2').setVisible(true);
             }
-            else {
-                Form.getControl("Subgrid_new_2").setVisible(false);
+            else if (Form.getAttribute('bsr_issubsequentstagedapp').getValue() == true) {
+                Form.getControl('Subgrid_new_2').setVisible(false);
             }
         }
     }
--- Code/BSR.D365.WS2/BSR.D365.WS2.WebResources/js/bsr_buildingdetails/bsr_buildingdetails.js
+++ Code/BSR.D365.WS2/BSR.D365.WS2.WebResources/js/bsr_buildingdetails/bsr_buildingdetails.js
@@ -3,57 +3,65 @@ var BuildingDetails;
 (function (BuildingDetails) {
     var Form;
     function ShowHideExistingHRBFields(executionContext) {
-        Form = executionContext.getFormContext();
-        if (Form.getAttribute("bsr_hrbworkid").getValue() == null &&
-            Form.getAttribute("bsr_existingbuildingworkid").getValue() == null) {
-            Form.getControl("bsr_currentprimaryuse").setVisible(false);
+        Form = (executionContext.getFormContext());
+        if (Form.getAttribute('bsr_hrbworkid').getValue() == null &&
+            Form.getAttribute('bsr_existingbuildingworkid').getValue() == null) {
+            Form.getControl('bsr_currentprimaryuse').setVisible(false);
         }
-        if (Form.getAttribute("bsr_hrbworkid").getValue() !== null) {
-            var hrbWork = Form.getAttribute("bsr_hrbworkid").getValue()[0].id;
+        if (Form.getAttribute('bsr_hrbworkid').getValue() !== null) {
+            var hrbWork = Form.getAttribute('bsr_hrbworkid').getValue()[0].id;
             if (hrbWork === '{6FA7A0F4-4047-EE11-BE6E-6045BDF1E965}') {
-                Form.getControl("bsr_existtypeworkproposal").setVisible(false);
-                Form.getControl("bsr_existingbuildingworkid").setVisible(false);
-                Form.getControl("bsr_currentprimaryuse").setVisible(false);
+                Form.getControl('bsr_existtypeworkproposal').setVisible(false);
+                Form.getControl('bsr_existingbuildingworkid').setVisible(false);
+                Form.getControl('bsr_currentprimaryuse').setVisible(false);
             }
             else if (hrbWork === '{57871F02-4147-EE11-BE6E-6045BDF1E965}') {
-                Form.getControl("bsr_existtypeworkproposal").setVisible(false);
-                Form.getControl("bsr_existingbuildingworkid").setVisible(false);
-                Form.getControl("bsr_currentprimaryuse").setVisible(true);
+                Form.getControl('bsr_existtypeworkproposal').setVisible(false);
+                Form.getControl('bsr_existingbuildingworkid').setVisible(false);
+                Form.getControl('bsr_currentprimaryuse').setVisible(true);
             }
         }
-        if (Form.getAttribute("bsr_existingbuildingworkid").getValue() !== null) {
-            var existingbuildingwork = Form.getAttribute("bsr_existingbuildingworkid").getValue()[0].id;
-            if (existingbuildingwork === '{C19E1C08-4147-EE11-BE6E-6045BDF1E965}' || (existingbuildingwork === '{089F1C08-4147-EE11-BE6E-6045BDF1E965}')) {
-                Form.getControl("bsr_existtypeworkproposal").setVisible(true);
-                Form.getControl("bsr_existingbuildingworkid").setVisible(true);
-                Form.getControl("bsr_hrbworkid").setVisible(false);
+        if (Form.getAttribute('bsr_existingbuildingworkid').getValue() !== null) {
+            var existingbuildingwork = Form.getAttribute('bsr_existingbuildingworkid').getValue()[0].id;
+            if (existingbuildingwork === '{C19E1C08-4147-EE11-BE6E-6045BDF1E965}' ||
+                existingbuildingwork === '{089F1C08-4147-EE11-BE6E-6045BDF1E965}') {
+                Form.getControl('bsr_existtypeworkproposal').setVisible(true);
+                Form.getControl('bsr_existingbuildingworkid').setVisible(true);
+                Form.getControl('bsr_hrbworkid').setVisible(false);
             }
         }
     }
     BuildingDetails.ShowHideExistingHRBFields = ShowHideExistingHRBFields;
     function LockFieldsOnSubmissionDate(executionContext) {
-        Form = executionContext.getFormContext();
-        if (Form.getAttribute("bsr_submissiondate").getValue() !== null) {
-            Form.getControl("bsr_existtypeworkproposal").setDisabled(true);
-            Form.getControl("bsr_primaryuse").setDisabled(true);
-            Form.getControl("bsr_currentprimaryuse").setDisabled(true);
+        Form = (executionContext.getFormContext());
+        if (Form.getAttribute('bsr_submissiondate').getValue() !== null) {
+            Form.getControl('bsr_existtypeworkproposal').setDisabled(true);
+            Form.getControl('bsr_primaryuse').setDisabled(true);
+            Form.getControl('bsr_currentprimaryuse').setDisabled(true);
         }
         else {
-            Form.getControl("bsr_existtypeworkproposal").setDisabled(false);
-            Form.getControl("bsr_primaryuse").setDisabled(false);
-            Form.getControl("bsr_currentprimaryuse").setDisabled(false);
+            Form.getControl('bsr_existtypeworkproposal').setDisabled(false);
+            Form.getControl('bsr_primaryuse').setDisabled(false);
+            Form.getControl('bsr_currentprimaryuse').setDisabled(false);
         }
     }
     BuildingDetails.LockFieldsOnSubmissionDate = LockFieldsOnSubmissionDate;
     function ShowHidePrescribedDocumentSection(executionContext) {
-        Form = executionContext.getFormContext();
-        var bsr_bcapplicationid = Form.getAttribute("bsr_bcapplicationid").getValue()[0].id;
-        Xrm.WebApi.retrieveRecord("bsr_buildingcontrolapplication", bsr_bcapplicationid, "?$select=bsr_stagedapplication,bsr_issubsequentstagedapp").then(function success(result) {
+        Form = (executionContext.getFormContext());
+        var bsr_bcapplicationid = Form.getAttribute('bsr_bcapplicationid').getValue()[0].id;
+        Xrm.WebApi.retrieveRecord('bsr_buildingcontrolapplication', bsr_bcapplicationid, '?$select=bsr_stagedapplication,bsr_issubsequentstagedapp').then(function success(result) {
             var bsr_stagedapplication = result.bsr_stagedapplication;
             var bsr_issubsequentstagedapp = result.bsr_issubsequentstagedapp;
-            if (bsr_stagedapplication == true || bsr_issubsequentstagedapp == true) {
-                Form.ui.tabs.get("tab1").sections.get("tab1_section_12").setVisible(false);
-                Form.ui.tabs.get("tab1").sections.get("tab1_section_16").setVisible(false);
+            if (bsr_stagedapplication == true ||
+                bsr_issubsequentstagedapp == true) {
+                Form.ui.tabs
+                    .get('tab1')
+                    .sections.get('tab1_section_12')
+                    .setVisible(false);
+                Form.ui.tabs
+                    .get('tab1')
+                    .sections.get('tab1_section_16')
+                    .setVisible(false);
             }
         }, function (error) {
             console.log(error.message);
