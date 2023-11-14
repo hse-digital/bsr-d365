784e2f5a8d8af1bcd93264cb1616fe1bff26b432
Malachy Keaveny Codec Systems <MKeaveny.codecsystems@hsegov.onmicrosoft.com>
2023-11-14 11:03:15 +0000

    Solution patch commit from WS2 CICD #16314 #17236 #17419 #17420 #17421 #17424 #17441 #17970 #18448 #18452 #18454

--- Customization/WS2/Managed/BuildingControlCore/WebResources/bsr_/buildingcontrol/tables/bsr_buildingcontrollifecycle/script/bsr_buildingcontrollifecycle.js
+++ Customization/WS2/Managed/BuildingControlCore/WebResources/bsr_/buildingcontrol/tables/bsr_buildingcontrollifecycle/script/bsr_buildingcontrollifecycle.js
@@ -2,7 +2,6 @@
 var BuildingControlLifecycle;
 (function (BuildingControlLifecycle) {
     function showHideCompletionTab(executionContext) {
-        debugger;
         var formContext = executionContext.getFormContext();
         var changeControl = formContext.getAttribute("bsr_changecontrol");
         if (changeControl !== null) {
@@ -16,5 +15,45 @@ var BuildingControlLifecycle;
         }
     }
     BuildingControlLifecycle.showHideCompletionTab = showHideCompletionTab;
+    function regulatoryDecision(executionContext) {
+        var formContext = executionContext.getFormContext();
+        var bccaApproval = formContext.getAttribute("bsr_bcaaapproval");
+        var bccaApprovalReq = formContext.getAttribute("bsr_bcaaapprovalrequirements");
+        var bccaRej = formContext.getAttribute("bsr_bcaarejection");
+        var confirmStrings = {
+            confirmButtonLabel: "Yes", text: "Are you sure you wish to continue with Reg Decision?", title: "Do you wish to continue?"
+        };
+        var confirmOptions = { height: 120, width: 260 };
+        function handleConfirm(attribute) {
+            Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(function (confirmationResult) {
+                console.log("Confirmation dialog closed");
+                if (confirmationResult.confirmed) {
+                    if (attribute) {
+                        attribute.setValue(true);
+                    }
+                }
+                else {
+                    if (attribute) {
+                        attribute.setValue(false);
+                    }
+                }
+            }, function (error) {
+                console.log(error.message);
+                if (attribute) {
+                    attribute.setValue(false);
+                }
+            });
+        }
+        if (bccaApproval !== null && bccaApproval.getValue() === true) {
+            handleConfirm(bccaApproval);
+        }
+        if (bccaApprovalReq !== null && bccaApprovalReq.getValue() === true) {
+            handleConfirm(bccaApprovalReq);
+        }
+        if (bccaRej !== null && bccaRej.getValue() === true) {
+            handleConfirm(bccaRej);
+        }
+    }
+    BuildingControlLifecycle.regulatoryDecision = regulatoryDecision;
 })(BuildingControlLifecycle || (BuildingControlLifecycle = {}));
 //# sourceMappingURL=bsr_buildingcontrollifecycle.js.map
